ca7403b0808fb6a50f84b9ac0e046184b06a4367
Kevin <kevinm1776@gmail.com>
2020-03-29 14:54:49 -0400

    Learned gaurds and errors.

--- app/Main.hs
+++ app/Main.hs
@@ -20,7 +20,18 @@ addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
 sumPairs :: [(Int, Int)] -> [Int]
 sumPairs ls = [a + b | (a, b) <- ls]
 
-
 firstLetter :: String -> String
 firstLetter "" = ""
 firstLetter word@(first:remaining) = "The first letter of " ++ word ++ " is " ++ [first]
+
+
+ageGroup :: Double -> String
+ageGroup age
+    | age < 0 = error "Age must be between 0 and 200"
+    | age > 200 = error "Age must be between 0 and 200" 
+    | age <= 1 = "Baby"
+    | age <= 3 = "Toddler"
+    | age < 13 = "Child"
+    | age < 18 = "Teen"
+    | age < 60 = "Adult"
+    | otherwise = "Elder"
