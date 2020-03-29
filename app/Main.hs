e97239f1405510e2d335260dc41d35d3ef83f34b
Kevin <kevinm1776@gmail.com>
2020-03-29 14:25:33 -0400

    Touple decomposition.

--- app/Main.hs
+++ app/Main.hs
@@ -12,3 +12,7 @@ lucky x = "Sorry, you're out of luck, pal."
 factorial :: Int -> Int
 factorial 0 = 1
 factorial n = n * factorial (n - 1)
+
+
+addVectors  :: (Double, Double) -> (Double, Double) -> (Double, Double)
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
