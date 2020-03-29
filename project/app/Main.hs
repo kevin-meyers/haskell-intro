d1c9a11886a935c7e81be43757f296749668ac7b
Kevin <kevinm1776@gmail.com>
2020-03-29 14:14:36 -0400

    Recursive function!

--- project/app/Main.hs
+++ project/app/Main.hs
@@ -8,3 +8,7 @@ main = putStrLn "Hello World!"
 lucky :: Int -> String
 lucky 7 = "LUCKY NUMBER SEVEN!"
 lucky x = "Sorry, you're out of luck, pal."
+
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
