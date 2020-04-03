38235fb1d7613e9e28b3cc1462829b826cd64541
Kevin <kevinm1776@gmail.com>
2020-04-03 13:42:20 -0400

    Added cuboid formulas.

--- app/Geometry/Sphere.hs
+++ app/Geometry/Sphere.hs
@@ -1,11 +1,10 @@
 module Geometry.Sphere
-    ( volume
-    , area
-    ) where
-
+  ( volume
+  , area
+  ) where
 
 volume :: Float -> Float
-volume = (* 4.0) . (/ 3) . (* pi) . (^3)
+volume = (* 4.0) . (/ 3) . (* pi) . (^ 3)
 
 area :: Float -> Float
-area = (* 4) . (* pi) . (^2)
+area = (* 4) . (* pi) . (^ 2)
