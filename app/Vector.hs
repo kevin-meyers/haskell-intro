7b1d8bd4124eae3b613b87c69867f0c672ee2e24
Kevin <kevinm1776@gmail.com>
2020-04-07 14:57:25 -0400

    Implemented Vector datatype.

---
+++ app/Vector.hs
@@ -0,0 +1,15 @@
+main :: IO ()
+main = putStrLn "help"
+
+data Vector a =
+  Vector a a a
+  deriving (Show)
+
+vPlus :: (Num a) => Vector a -> Vector a -> Vector a
+(Vector i j k) `vPlus` (Vector l m n) = Vector (i + l) (j + m) (k + n)
+
+dotProd :: (Num a) => Vector a -> Vector a -> Vector a
+(Vector i j k) `dotProd` (Vector l m n) = Vector (i * l) (j * m) (k * n)
+
+vMult :: (Num a) => Vector a -> a -> Vector a
+(Vector i j k) `vMult` m = Vector (i * m) (j * m) (k * m)
